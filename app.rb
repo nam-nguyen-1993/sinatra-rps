d6ac7126bff647d6a60f2023f27cb3cce761a987
nam-nguyen-1993 <nam.nguyen@chicagobooth.edu>
2025-01-30 15:46:42 +0000

    Final

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get ("/rock") do
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "tied"
+  elsif @comp_moves == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+
+  erb(:play_rock)
+
+end
+
+get ("/paper") do
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "won"
+  elsif @comp_moves == "paper"
+    @outcome = "tied"
+  else 
+    @outcome = "lost"
+  end
+
+  erb(:play_paper)
+
+end
+
+get ("/scissors") do
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "lost"
+  elsif @comp_moves == "paper"
+    @outcome = "won"
+  else 
+    @outcome = "tied"
+  end
+
+  erb(:play_scissors)
+
 end
